7787897b2c584c57344dc85c6d8e32d6c4b6d333
Eric Nguyen <ericnguyen.adl@gmail.com>
2020-03-29 06:03:55 +1030

    Create ContactDetails.cs in the Model package

---
+++ StartFinanceMaster/InstaRichie/Models/ContactDetails.cs
@@ -0,0 +1,24 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    class ContactDetails
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [Unique]
+        public string Name { get; set; }
+
+        [NotNull]
+        public string TelNum { get; set; }
+
+        [NotNull]
+        public string Address { get; set; }
+    }
+}
